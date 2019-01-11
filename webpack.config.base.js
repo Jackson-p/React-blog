237ab660628d4b7a7ecec2083a87f1fe5439082e
Jackson-p <1216884492@qq.com>
2019-01-11 21:11:46 +0800

    aftertreeshaking

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -46,10 +46,24 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         publicPath: '',
     },
     plugins: [
         
-    ]
+    ],
+    optimization: {
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              // 抽离第三方插件
+              test: /node_modules/, // 指定是node_modules下的第三方包
+              chunks: 'initial',
+              name: 'vendor', // 打包后的文件名，任意命名
+              // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
+              priority: 10
+            }
+          }
+        }
+      }
 };
