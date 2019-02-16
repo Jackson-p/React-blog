e8c4c11f6146a7782ec31e484a713579f0f5b580
Jackson-p <1216884492@qq.com>
2019-02-16 22:41:46 +0800

    'fixloading'

--- src/js/components/learn.js
+++ src/js/components/learn.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
-import LearnBody from './learnbody';
-import { Col, Row, Spin} from 'antd';
+import LearnItem from './learnitem';
+import { Col, Row, Spin, Pagination} from 'antd';
+import { getIssues, calcPagetotal } from '@/utils/utils';
 import '../../css/learn.css';
+import '../../css/learnbody.css';
 
 
 // interceptors.request.use((req) => {
@@ -20,26 +22,67 @@ export default class Learn extends React.Component{
     constructor(){
         super();
         this.state = {
-            chosentag:'ALL',
-            loading:false
+            chosentag : 'ALL',
+            loading : false,
+            learnlist : '加载中...',
+            currentpage : 1,
+            totalpages : -1//代表分页器还未渲染
         }
-        
+        this._isMounted = true;
+        this.pagesize = 6;
+    }
+    showIssues(label='',currentpage = 1, pagesize=this.pagesize, keyword=""){
+        label = label == 'ALL'?'':label;
+        this.setState({loading:true});
+        getIssues({
+            label:label,
+            currentpage:currentpage,
+            pagesize:pagesize,
+            keyword:keyword
+        }).then((response) => {
+            let data = response.data.items;
+            let totalpages = calcPagetotal(response.data.total_count, pagesize);
+            if(!data || data.length ==0){
+                data = <h1>暂无数据哦</h1>
+            }
+            if(this._isMounted){
+                this.setState({totalpages:totalpages, learnlist:data, loading:false});
+            }
+        }).catch(e => console.log(e));
     }
     setTag(val){
-        this.setState({chosentag:val});
+        this.setState({chosentag:val, learnlist:'加载中...', currentpage : 1, totalpages : -1 }
+         ,this.showIssues(val));
     }
-    handleLoading(loadingbool){
-        console.log(loadingbool);
-        //this.setState({loading:loadingbool});
+    handlePages(page){
+        this.setState({learnlist:'加载中...', currentpage : page}
+         ,this.showIssues(this.state.chosentag ,page));
+    }
+    componentDidMount(){
+        this.showIssues(this.state.chosentag);
+    };
+    componentWillUnmount(){
+        this._isMounted = false;
     }
     render(){
         const tagNames = ['HTML', 'CSS', 'JS专题', 'Git', '工具、库与框架', 'Web前端安全', 'Web前端测试', '前端面试', '后端', '资源环境', '计算机科学'];
         //请求太耗时了，能省就省吧。
-        const chosentag = this.state.chosentag;
-        const tagall = <a id="all" className={chosentag == 'ALL' ? "alltagchecked" :  null} onClick={this.setTag.bind(this, 'ALL')} >show all</a>;
-        const taglist = tagNames.map((val, index) => {
+        const reg = /[\#\`{3}\*]i/g;
+        let chosentag = this.state.chosentag;
+        let tagall = <a id="all" className={chosentag == 'ALL' ? "alltagchecked" :  null} onClick={this.setTag.bind(this, 'ALL')} >show all</a>;
+        let taglist = tagNames.map((val, index) => {
             return <a key={index} className={chosentag == val ?"tagchecked":null} onClick={this.setTag.bind(this, val)}>{ val }</a>;
         });
+        let contentBefore,content;
+        let learnlist = this.state.learnlist;
+        let Learnlist = Array.isArray(learnlist) ?
+        learnlist.map((article,index) => {
+                contentBefore = article.body.replace(reg,"");
+                content = contentBefore.substring(0,200)+"...";
+                return <LearnItem key={index} title={article.title} subtitle={content} num={article.number} />      
+        }) 
+        :
+        learnlist;
         return(
             <Spin spinning={this.state.loading} size="large">
                 <Header selectedhead={2}/>
@@ -52,7 +95,10 @@ export default class Learn extends React.Component{
                         </Col>
                         <Col md={14} span={24} className="tag">{ chosentag }</Col>
                         <Col md={{span:14, offset:1}} span={24}>
-                            <LearnBody tagName={ chosentag } handleLoading={ this.handleLoading.bind(this) } />
+                            <div className="learn-list">{ Learnlist }</div>
+                            <div className="learn-footer">
+                                {this.state.totalpages >= 0 && <Pagination defaultCurrent={1} onChange={this.handlePages.bind(this)} total={this.state.totalpages*10} />}
+                            </div>
                         </Col>
                     </Row>
                 </Col>
